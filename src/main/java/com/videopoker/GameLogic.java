6642359ed0553eadec7003e41ce447c7c848d96c
AB1775 <austin.booth9795@yahoo.com>
2025-05-06 08:23:49 -0500

    Add GameState enum and comments for game state restrictions

--- src/main/java/com/videopoker/GameLogic.java
+++ src/main/java/com/videopoker/GameLogic.java
@@ -1,5 +1,15 @@
 package com.videopoker;
 
 public class GameLogic {
-
+    /* Game State Restrictions
+     *  WAITING_FOR_BET: Player Must Place Valid Bet Before Initial Hand is Dealt
+     *  FIRST_DRAW: Player Must Hold 0-5 Cards, No Bets Placed, 1 Draw Remaining, No Payout
+     *  FINAL_DRAW: Nonheld Cards are Replaced, Player Cannot Hold or Unhold Cards from This Point, No Bets are Placed, 0 Draws Remain
+     * ** ENSURE THAT STATE TRANSITIONS ARE VALID **
+     */
+    public enum GameState {
+        WAITING_FOR_BET,          // Player Places a Bet
+        FIRST_DRAW,               // Initial 5-Card Hand is Dealt
+        FINAL_DRAW,               // Player Has Chosen Hold Cards and is Dealt New Cards Hand is Evaluated and Bankroll is Updated
+    }
 }
