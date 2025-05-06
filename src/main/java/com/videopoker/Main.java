4c6ccbc8d05714272c549f3c59c7b08e39ada239
AB1775 <austin.booth9795@yahoo.com>
2025-05-06 08:20:20 -0500

    Refactor main method to improve GUI initialization

--- src/main/java/com/videopoker/Main.java
+++ src/main/java/com/videopoker/Main.java
@@ -1,7 +1,12 @@
 package com.videopoker;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // Schedule a job to ensure that the GUI is created on the Event Dispatch Thread (EDT)
+        SwingUtilities.invokeLater(() -> {
+            // Initialize GameLogic and SwingGUI Here
+        });
     }
 }
