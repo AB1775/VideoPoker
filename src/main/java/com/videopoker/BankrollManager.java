5e5c2e6f69d6cff16d9f38e3696b1bceae404b05
AB1775 <austin.booth9795@yahoo.com>
2025-05-06 09:43:37 -0500

    Add payoutMap to map hand titles to payout values

--- src/main/java/com/videopoker/BankrollManager.java
+++ src/main/java/com/videopoker/BankrollManager.java
@@ -1,5 +1,20 @@
 package com.videopoker;
 
-public class BankrollManager {
+import java.util.Map;
+import java.util.HashMap;
 
+public class BankrollManager {
+    Map<String, Integer> payoutMap = new HashMap<>( // Make Immutable
+        Map.of(
+            "Royal Flush", 250,
+            "Straight Flush", 50,
+            "Four of a Kind", 25,
+            "Full House", 9,
+            "Flush", 6,
+            "Straight", 4,
+            "Three of a Kind", 3,
+            "Two Pair", 2,
+            "Jacks or Better", 1
+        )
+    );
 }
