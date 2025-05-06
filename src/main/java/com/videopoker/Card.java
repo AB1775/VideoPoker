1f87519c55d248271e7013f520ddd0f3509e9bbe
AB1775 <austin.booth9795@yahoo.com>
2025-05-06 08:26:46 -0500

    Add card values and suits lists

--- src/main/java/com/videopoker/Card.java
+++ src/main/java/com/videopoker/Card.java
@@ -1,5 +1,15 @@
 package com.videopoker;
 
-public class Card {
+import java.util.List;
 
+public class Card {
+    public Object cardValue;
+    public String cardSuit;
+
+    // Possible Card Values and Suits: Used an Object to Include Multiple Data Type
+    List<Object> cardValuesList = List.of('A', 2, 3, 4, 5,
+                                           6, 7, 8, 9, 10,
+                                          'J', 'Q', 'K');
+    
+    List<Character> cardSuitsList = List.of('H', 'D', 'C', 'S');
 }
